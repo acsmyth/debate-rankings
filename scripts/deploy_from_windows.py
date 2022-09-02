2e708dd59ed4f9dc31139f78c1c5831996fefe69
ACSmyth <smyth.a@northeastern.edu>
2022-09-02 00:14:31 -0400

    Add logs

--- scripts/deploy_from_windows.py
+++ scripts/deploy_from_windows.py
@@ -14,14 +14,16 @@ def run_commands_over_ssh(*commands):
 
 # Deploy DB
 scp('../data-collection/debate.db', '/home/bitnami/debate-rankings/data-collection/')
+print('Deployed database!', '\n')
 
 # Build and deploy client
 os.system('cd ../site/client/ && npm install')
 os.system(f'npm run build --prefix ../site/client/')
 scp('../site/client/build/', '/home/bitnami/debate-rankings/site/client/', recursive=True)
 run_commands_over_ssh('sudo systemctl restart nginx')
+print('Deployed front-end!', '\n')
 
-# Build and deploy server
+# Deploy server
 scp('../site/server/bin/', '/home/bitnami/debate-rankings/site/server/', recursive=True)
 scp('../site/server/public/', '/home/bitnami/debate-rankings/site/server/', recursive=True)
 scp('../site/server/routes/', '/home/bitnami/debate-rankings/site/server/', recursive=True)
@@ -33,3 +35,4 @@ run_commands_over_ssh(
   'pm2 stop www',
   'PORT=3000 pm2 start ~/debate-rankings/site/server/bin/www'
 )
+print('Deployed back-end!', '\n')
