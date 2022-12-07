5667df7afe3fd4e080431c301a0bde9713a6b626
ACSmyth <smyth.a@northeastern.edu>
2022-12-06 21:00:00 -0500

    Update data collection for both OSes

--- data-collection/run_data_collection_process.py
+++ data-collection/run_data_collection_process.py
@@ -2,7 +2,9 @@ import os
 
 start = '2022-08-01'
 end = '2023-08-01'
-sleep_time = 4
+sleep_time = 3
+PYTHON = 'python3' if os.name == 'posix' else 'python'
+
 
 # Scrape tournament ids
 os.system(f'npx cypress run --spec "cypress/integration/scrape_tournament_ids.js" --env start="{start}",end="{end}"')
@@ -10,8 +12,8 @@ os.system(f'npx cypress run --spec "cypress/integration/scrape_tournament_ids.js
 # # Scrape entries from each tournament
 os.system(f'npx cypress run --spec "cypress/integration/scrape_bid_tournaments.js" --env start="{start}",end="{end}"')
 
-# Get round pages for each entry
-os.system(f'python get_round_pages.py start="{start}" end="{end}" sleep={sleep_time}')
+# # Get round pages for each entry
+os.system(f'{PYTHON} get_round_pages.py start="{start}" end="{end}" sleep={sleep_time}')
 
-# Process round pages for each entry
-os.system(f'python process_round_pages.py start="{start}" end="{end}"')
+# # Process round pages for each entry
+os.system(f'{PYTHON} process_round_pages.py start="{start}" end="{end}"')
